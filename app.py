2b028ef22e5abfc4c6bc74102d0b78140d88f363
LYZ-Li <liyuanzhemail@gmail.com>
2024-06-13 21:40:37 +0200

    update

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 import gradio as gr
-from lmdeploy.vl import load_image
-from lmdeploy import pipeline, TurbomindEngineConfig
+import os
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 
+# download internlm2 to the base_path directory using git tool
+base_path = './internlm-chat-7b-w4'
+os.system(f'git clone https://code.openxlab.org.cn/LMDeploy/internlm-chat-7b-w4.git {base_path}')
+os.system(f'cd {base_path} && git lfs pull')
 
-backend_config = TurbomindEngineConfig(session_len=8192) # 图片分辨率较高时请调高session_len
-pipe = pipeline('liuhaotian/llava-v1.6-vicuna-7b', backend_config=backend_config) 
+tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16).to(device)
 
-def model(image, text):
-    if image is None:
-        return [(text, "请上传一张图片。")]
-    else:
-        response = pipe((text, image)).text
-        return [(text, response)]
+def chat(message,history):
+    for response,history in model.stream_chat(tokenizer,message,history,max_length=2048,top_p=0.7,temperature=1):
+        yield response
 
-demo = gr.Interface(fn=model, inputs=[gr.Image(type="pil"), gr.Textbox()], outputs=gr.Chatbot())
-demo.launch() 
+gr.ChatInterface(chat,
+                 title="InternLM2-Chat-7B-w4",
+                description="""
+InternLM is mainly developed by Shanghai AI Laboratory.  
+                 """,
+                 ).queue(1).launch()
